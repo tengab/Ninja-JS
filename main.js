8bda5a29cd656fa83f607074171a6586a0c695b0
Tomasz Banach <tomaszbanach01@gmail.com>
2017-11-17 14:38:21 +0100

    lesson 17

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var age = 5;
+var links = document.getElementsByTagName("a");
 
-while (age < 10) {
+for (i = 1; i <= links.length; i++) {
 
-    console.log("Your age is less than 10")
-    age++;
+    console.log("this is link number" + i);
+
 }
 
-document.write("you are now over 10")
+document.write("all links now looped");
