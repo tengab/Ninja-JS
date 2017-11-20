5b6f67fdc08b6850e0dca6a37b773a50d7d65c24
Tomasz Banach <tomaszbanach01@gmail.com>
2017-11-20 11:23:59 +0100

    lesson 29

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
-var myArray = new Array();
-
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Tomek";
-myCar.drive = function(){
-    console.log("now driving");
-};
-
-myCar.drive();
-
 var myCar2 = {
 
     maxSpeed: 50,
     driver: "Magda",
     drive: function(speed, time){
-        return(speed * time);
+        console.log(speed * time);
+    },
+    test: function(){
+        console.log(this);
     }
 };
 
-console.log(myCar.maxSpeed);
-var travel = myCar2.drive(50, 3);
-console.log(travel);
-console.log("distance travel was " + travel);
+myCar2.test();
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
