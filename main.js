61324501543119965be57852948d77b1609d060b
Tomasz Banach <tomaszbanach01@gmail.com>
2017-11-20 11:16:53 +0100

    lesson 28

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-var string1 = "Abc";
-var string2 = "Bcd";
+var myArray = new Array();
 
-console.log(string1 < string2);
+myArray[0] = 8;
+myArray[1] = "hello";
+
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Tomek";
+myCar.drive = function(){
+    console.log("now driving");
+};
+
+myCar.drive();
+
+var myCar2 = {
+
+    maxSpeed: 50,
+    driver: "Magda",
+    drive: function(speed, time){
+        return(speed * time);
+    }
+};
+
+console.log(myCar.maxSpeed);
+var travel = myCar2.drive(50, 3);
+console.log(travel);
+console.log("distance travel was " + travel);
