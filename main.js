d6e3a2c022fe4c9cbf6fad49082500b5de9b6d20
Tomasz Banach <tomaszbanach01@gmail.com>
2017-11-20 09:15:56 +0100

    lesson 21

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-function getAverage (a,b,c,d,e,f){
+function getAverage (a,b){
 
-    var average = (a + b + c + d + e + f) / 6;
+    var average = (a + b) / 2;
     console.log(average);
     return average;
 }
 
-var myResult = getAverage(7,8,9,10,11,12);
+var myResult = getAverage(7,11);
 console.log("the average is " + myResult);
+
+function logResult(){
+
+    console.log("the average is " + myResult);
+
+}
+
+logResult();
